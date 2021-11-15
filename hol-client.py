e540353d3dfdf86adbe003319c63188adaf353fc
gavmac00 <macdonga@tcd.ie>
2021-11-15 13:16:24 +0000

    autonomy

--- hol-client.py
+++ hol-client.py
@@ -1,45 +1,64 @@
 #version 2.1 of higher or lower game - Gavin MacDonnell 14/11/21
 
-#this file contains the original higher or lower 2 game,
+#this file contains base code for the original higher or lower 2 game,
 # but modifications have been made such that it works
 # alongside hol-player.py (an autonomous player)
 
 import random as random
-
+import time
 #instructions
 print("\n\nA game of Higher or Lower. Each round, press 'h' for \"Higher\", press 'l' for \"Lower\".")
 
 status = 1 #the current player status (playing = 1, lost = 0)
 score = 0 #rounds completed 
 
-#starting number is displayed to user
+#starting number is displayed to user & written in hol-data.txt
 old = random.randint(1,100)
 print("The starting number is " + str(old))
 
+
+give = "new: " + str(old) + "\nend"
+with open('hol-data.txt', 'w') as wd:
+    wd.write(give)
+    wd.close()
+
 #prompts for higher or lower
-choice = input("Higher or Lower? ")
+print("Higher or Lower? ") #changed input to print here (so as to not hold up the console)
 
-while status == 1:
-    new = random.randint(1,100)
-    print("\n")
-    if choice == "h":
-        if new > old:
-            print(str(new) + " is higher than " + str(old) + ".")
-            score = score + 1
-        else: 
-            print(str(new) + " is not higher than " + str(old) + ".")
-            status = 0
-            print("Game over, Score: " + str(score))
-            break
+time. sleep(5)
 
-    if choice == "l":
-        if new < old:
-            print(str(new) + " is lower than " + str(old) + ".")
-            score = score + 1
-        else:
-            print(str(new) + " is not lower than " + str(old) + ".")
-            status = 0
-            print("Game over, Score: " + str(score))            
-            break
-    choice = input("Higher or Lower? ")
-    old = new
+with open('hol-data.txt', 'r') as rd:
+    has_been_changed = rd.read()
+    rd.close()
+
+print(has_been_changed)
+
+if  has_been_changed[1] == "v":
+    print("indexing correct")
+else:
+    print("indexing incorrect")
+
+# while status == 1:
+#     new = random.randint(1,100)
+#     print("\n")
+#     if choice == "h":
+#         if new > old:
+#             print(str(new) + " is higher than " + str(old) + ".")
+#             score = score + 1
+#         else: 
+#             print(str(new) + " is not higher than " + str(old) + ".")
+#             status = 0
+#             print("Game over, Score: " + str(score))
+#             break
+
+#     if choice == "l":
+#         if new < old:
+#             print(str(new) + " is lower than " + str(old) + ".")
+#             score = score + 1
+#         else:
+#             print(str(new) + " is not lower than " + str(old) + ".")
+#             status = 0
+#             print("Game over, Score: " + str(score))            
+#             break
+#     choice = input("Higher or Lower? ")
+#     old = new
--- hol-player.py
+++ hol-player.py
@@ -2,47 +2,83 @@
 # or lower game automomously, lets see how it goes - Gavin MacDonnell 14/11/21
 
 import random as random
+import time
 
-#read the text file
-with open('hol-data.txt') as rd:
-    alltext = rd.read()
+def player():
+    time.sleep(4)
+    new = "v"
+    while not new.isdigit(): # leaves this loop only when correct data is present
+        #read the text file
+        with open('hol-data.txt') as rd:
+            alltext = rd.read()
 
-#do something with the information
-new_u = alltext[5] #units
-new_t = alltext[6] #tens
-new_h = alltext[7] #hundred (if == 100)
+        #do something with the information
+        new_u = alltext[5] #units
+        new = new_u
 
-new = new_u
+        new_t = alltext[6] #tens
+        new_h = alltext[7] #hundred (if == 100)
 
-#append the charachters
-if new_t.isdigit():
-    new = new_u + new_t
+        #append the charachters
+        if new_t.isdigit():
+            new = new_u + new_t
 
-if new_h.isdigit():
-    new = new_u + new_t + new_h
+        if new_h.isdigit():
+            new = new_u + new_t + new_h
+                
+    new = int(new) #at this point, the string is certain it can be a digit, simply assigning it here and now the player knows what the 'new' value is
 
-print("new: " + new)
+    while not alltext[0] == "-":
+        new = "v"
+        #read the text file
+        with open('hol-data.txt') as rd:
+            alltext = rd.read()
 
-new = int(new) #at this point, player knows what the new value is
+        #do something with the information
+        new_u = alltext[5] #units
+        new = new_u
 
-#now to enter whether we want to guess higher or lower
-h = "h"
-l = "l"
+        new_t = alltext[6] #tens
+        #new_h = alltext[7] #hundred (if == 100)
 
-if new < 50:
-    vote = h
+        #append the charachters
+        if new_t.isdigit():
+            new = new_u + new_t
 
-elif new > 50:
-    vote = l
+        if new_h.isdigit():
+            new = new_u + new_t + new_h
+        
+        new = int(new) #at this point, the string is certain it can be a digit, simply assigning it here and now the player knows what the 'new' value is
+        print("     Player understood 'new' value as: " + str(new) +".")
 
-else:
-    pr = random.randint(1,2)
-    if pr == 1:
-        vote = h
-    else:
-        vote = l
+        #now to enter whether we want to guess higher or lower
+        h = "h"
+        l = "l"
 
-#at this point, vote is chosen
-send = "vote: " + vote
-with open('hol-data.txt', 'w') as wd:
-    wd.write(send) # vote is sent, now on to the client
+        if new < 50:
+            vote = h
+
+        elif new > 50:
+            vote = l
+
+        else:
+            pr = random.randint(1,2)
+            if pr == 1:
+                vote = h
+            else:
+                vote = l
+
+        #at this point, vote is chosen
+        send = "vote: " + vote
+        with open('hol-data.txt', 'w') as wd:
+            wd.write(send)
+            wd.close()
+        
+        print("     Player cast vote of: " + vote) # vote is sent, now on to the client
+        time.sleep(8)
+
+        with open('hol-data.txt') as rd:
+            alltext = rd.read()
+        new = str(new)
+
+    quit()
